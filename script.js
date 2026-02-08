30d2042ebfa634e0efa18f1823bf43a23e7f3f13
Benedict Fels <benedictfels@web.de>
2026-02-08 16:04:19 +0100

    search function added

--- script.js
+++ script.js
@@ -10,97 +10,134 @@ dialogDiv.addEventListener('click', (event) => event.stopPropagation());
 
 const pokemonLocation = document.getElementById("content");
 const dialogContentRef = document.getElementById("dialog-contentID");
-const statsRef = document.getElementById("statsID");
-let pokemonArray = [];
 let pokemonObject = {};
 let currentNumberPokemon = 0;
 
-async function loadAPI() {
-    let pokemonFullAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/`);
-    let pokemonFullAPIAsJson = await pokemonFullAPI.json();
-    console.log("Full", pokemonFullAPIAsJson);
-    let pokemonAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/2`);
-    let pokemonAPIAsJson = await pokemonAPI.json();
-    console.log(pokemonAPIAsJson);
-    loadSpeciesAPI();
-    loadEvoAPI();
-    load134API();
-    loadSpecies134API();
-    loadEvo134API();
-}
+// async function loadAPI() {
+//     let pokemonFullAPI = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1024`);
+//     let pokemonFullAPIAsJson = await pokemonFullAPI.json();
+//     console.log("Full", pokemonFullAPIAsJson);
+//     let pokemonAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/2`);
+//     let pokemonAPIAsJson = await pokemonAPI.json();
+//     console.log(pokemonAPIAsJson);
+//     loadSpeciesAPI();
+//     loadEvoAPI();
+//     load134API();
+//     loadSpecies134API();
+//     loadEvo14API()
+//     loadEvo134API();
+// }
 
-async function loadSpeciesAPI() {
-    let pokemonSpeciesAPI = await fetch(`https://pokeapi.co/api/v2/pokemon-species/25/`);
-    let pokemonSpeciesAPIAsJson = await pokemonSpeciesAPI.json();
-    console.log("Species25", pokemonSpeciesAPIAsJson);
-}
+// async function loadSpeciesAPI() {
+//     let pokemonSpeciesAPI = await fetch(`https://pokeapi.co/api/v2/pokemon-species/10202/`);
+//     let pokemonSpeciesAPIAsJson = await pokemonSpeciesAPI.json();
+//     console.log("Species10202", pokemonSpeciesAPIAsJson);
+// }
 
-async function loadEvoAPI() {
-    let pokemonEvoAPI = await fetch(`https://pokeapi.co/api/v2/evolution-chain/10/`);
-    let pokemonEvoAPIAsJson = await pokemonEvoAPI.json();
-    console.log("EvoChain10", pokemonEvoAPIAsJson);
-}
+// async function loadEvoAPI() {
+//     let pokemonEvoAPI = await fetch(`https://pokeapi.co/api/v2/evolution-chain/10/`);
+//     let pokemonEvoAPIAsJson = await pokemonEvoAPI.json();
+//     console.log("EvoChain10", pokemonEvoAPIAsJson);
+// }
 
-async function load134API() {
-    let pokemonEvoAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/134/`);
-    let pokemonEvoAPIAsJson = await pokemonEvoAPI.json();
-    console.log("134", pokemonEvoAPIAsJson);
-}
+// async function load134API() {
+//     let pokemonEvoAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/30/`);
+//     let pokemonEvoAPIAsJson = await pokemonEvoAPI.json();
+//     console.log("30", pokemonEvoAPIAsJson);
+// }
 
-async function loadSpecies134API() {
-    let pokemonSpeciesAPI = await fetch(`https://pokeapi.co/api/v2/pokemon-species/134/`);
-    let pokemonSpeciesAPIAsJson = await pokemonSpeciesAPI.json();
-    console.log("Species134", pokemonSpeciesAPIAsJson);
-}
+// async function loadSpecies134API() {
+//     let pokemonSpeciesAPI = await fetch(`https://pokeapi.co/api/v2/pokemon-species/35/`);
+//     let pokemonSpeciesAPIAsJson = await pokemonSpeciesAPI.json();
+//     console.log("Species35", pokemonSpeciesAPIAsJson);
+// }
 
-async function loadEvo134API() {
-    let pokemonEvoAPI = await fetch(`https://pokeapi.co/api/v2/evolution-chain/67/`);
-    let pokemonEvoAPIAsJson = await pokemonEvoAPI.json();
-    console.log("EvoChainEevee", pokemonEvoAPIAsJson);
-}
+// async function loadEvo14API() {
+//     let pokemonEvoAPI = await fetch(`https://pokeapi.co/api/v2/evolution-chain/14/`);
+//     let pokemonEvoAPIAsJson = await pokemonEvoAPI.json();
+//     console.log("EvoChain14", pokemonEvoAPIAsJson);
+// }
+
+// async function loadEvo134API() {
+//     let pokemonEvoAPI = await fetch(`https://pokeapi.co/api/v2/evolution-chain/67/`);
+//     let pokemonEvoAPIAsJson = await pokemonEvoAPI.json();
+//     console.log("EvoChainEevee", pokemonEvoAPIAsJson);
+// }
 
 async function getMorePokemon() {
     document.body.classList.add('disable-interaction');
     const pantomimeRef = document.getElementById('pantomimeID');
-    pokemonLocation.classList.add("display-none");
+    pokemonLocation.style.visibility = "hidden";
     pantomimeRef.classList.remove("display-none");
     let myIndex = currentNumberPokemon
     for (let index = myIndex + 1; index < myIndex + 25; index++) {
-        await getPokemon(index)
+        await getPokemon(index);
+        currentNumberPokemon += 1;
     }
     pantomimeRef.classList.add("display-none");
-    pokemonLocation.classList.remove("display-none");
+    pokemonLocation.style.visibility = "visible";
     document.body.classList.remove('disable-interaction');
 }
 
 async function getPokemon(index) {
-    await fetchPokemon(index);
-    await loadPokemon(index);
+        await fetchPokemon(index);
+        loadPokemon(index); 
 }
 
-async function loadPokemon(index) {
+function loadCurrentPokemon() {
+    pokemonLocation.innerHTML ="";
+    let currentPokemon = currentNumberPokemon;
+    for (let i = 1; i <= currentPokemon; i++) {
+        loadPokemon(i);
+    }
+    return currentNumberPokemon = currentPokemon;
+}
 
+async function preloadSearchList() {
+    const data = await (await fetch('https://pokeapi.co/api/v2/pokemon?limit=1024')).json();
+    // const data = await response.json();
+    allPokemonSearchList = data.results.map((pokemon) => ({
+        name: pokemon.name,
+        id: cutOutIndex(pokemon.url)
+    }));
+}
+
+function loadPokemon(index) {
     pokemonLocation.innerHTML += pokemonTemplate(pokemonObject[index], index);
     getTypesBackground(pokemonObject[index]);
+}
 
-    currentNumberPokemon += 1;
+async function searchPokemon(searchInput) {
+    const searchTerm = searchInput.toLowerCase();
+    if (searchTerm.length === 0) {
+        loadCurrentPokemon();
+    }
+    if (searchTerm.length < 3) {
+        return;
+    }
+    const filtered = allPokemonSearchList.filter(p =>
+        p.name.includes(searchTerm) || p.id.toString() === searchTerm
+    );
+    pokemonLocation.innerHTML = "";
+    // filtered.forEach(p => {
+        for (const pokemon of filtered) {
+            await getPokemon(pokemon.id);
+        };
 }
 
 async function fetchPokemon(index) {
-    let pokemonAPI = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${index}`)).json();
-
-    // let pokemonSpeciesAPIAsJson = await (await fetch(`https://pokeapi.co/api/v2/pokemon-species/${index}`)).json();
-    let newPokemon = {
-        id: index,
-        name: pokemonAPI.species.name,
-        // flavorText: getDescription(pokemonSpeciesAPIAsJson.flavor_text_entries),
-        sprite: pokemonAPI.sprites.other["official-artwork"].front_default,
-        types: pokemonAPI.types,
-        stats: pokemonAPI.stats,
-        abilitiesURL: pokemonAPI.abilities,
-        // abilities: abilitiesData
-    };
-    pokemonObject[index] = newPokemon;
+    if (!pokemonObject[index]) {
+        let pokemonAPI = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${index}`)).json();
+        let newPokemon = {
+            id: index,
+            name: pokemonAPI.species.name,
+            sprite: pokemonAPI.sprites.other["official-artwork"].front_default,
+            types: pokemonAPI.types,
+            stats: pokemonAPI.stats,
+            abilitiesURL: pokemonAPI.abilities,
+        };
+        pokemonObject[index] = newPokemon;
+    }
 }
 
 function getTypesImage(types) {
@@ -179,31 +216,34 @@ function loadDescription() {
 
 function loadAbilities() {
     let pokemon = pokemonObject[currentIndex];
-    let html = `<p>Abilities:</p>` + pokemon.abilities.map(pokeAbility => `
-                            <p>${stringToCapital(pokeAbility.abilityName)}: ${pokeAbility.abilityDescription}</p>
+    let html = `<p>Abilities:</p> <ul>`
+        + pokemon.abilities.map(pokeAbility => `
+                            <li>${stringToCapital(pokeAbility.abilityName)}: ${pokeAbility.abilityDescription}</li>
                             `
-    ).join('');
+        ).join('') + `</ul>`;
     dialogContentRef.innerHTML += html
 }
 
 async function getAbilities() {
-    let abilitiesData = [];
-    for (const abilitySlot of pokemonObject[currentIndex].abilitiesURL) {
-        let abilityAPI = await (await fetch(`${abilitySlot.ability.url}`)).json();
-        let effectEntry = abilityAPI.effect_entries.find(entry => entry.language.name === 'en');
-        let abilityDetail = {
-            abilityName: abilitySlot.ability.name,
-            abilityDescription: effectEntry ? effectEntry.short_effect : "No description available"
-        };
-        abilitiesData.push(abilityDetail);
+    if (!pokemonObject[currentIndex].abilities) {
+        let abilitiesData = [];
+        for (const abilitySlot of pokemonObject[currentIndex].abilitiesURL) {
+            let abilityAPI = await (await fetch(`${abilitySlot.ability.url}`)).json();
+            let effectEntry = abilityAPI.effect_entries.find(entry => entry.language.name === 'en');
+            let abilityDetail = {
+                abilityName: abilitySlot.ability.name,
+                abilityDescription: effectEntry ? effectEntry.short_effect : "No description available"
+            };
+            abilitiesData.push(abilityDetail);
+        }
+        pokemonObject[currentIndex].abilities = abilitiesData;
     }
-    pokemonObject[currentIndex].abilities = abilitiesData;
 }
 
 function loadStats() {
-    statsRef.classList.add("red-underline");
-    dialogContentRef.classList.remove('evo-content')
+    // statsRef.classList.add("red-underline");
     dialogContentRef.innerHTML = "";
+    updateDialogNav('stats');
     pokemonObject[currentIndex].stats.forEach(pokeStat => {
         dialogContentRef.innerHTML += `
         <div class="dialog-stat-div">
@@ -219,16 +259,29 @@ async function aboutClick() {
     await getAbilities();
     await getDialogData();
     dialogContentRef.innerHTML = "";
-    dialogContentRef.classList.remove('evo-content');
-    dialogContentRef.classList.add('about-content')
+    updateDialogNav('about');
     loadDescription();
-    loadAbilities()
+    loadAbilities();
 }
 
 async function evolutionClick() {
     dialogContentRef.innerHTML = `<div class="loader">Loading evolution...</div>`;
     await getDialogData();
+    updateDialogNav('evolution')
     loadEvolution();
+}
+
+function updateDialogNav(content) {
+    dialogContentRef.classList.remove('evo-content');
+    dialogContentRef.classList.remove('about-content');
+    dialogContentRef.classList.add(`${content}-content`);
+    const navItems = document.querySelectorAll('.nav-item');
+    navItems.forEach(item => {
+        item.classList.remove("red-underline");
+        if (item.id === `${content}ID`) {
+            item.classList.add("red-underline");
+        }
+    });
 }
 
 function loadEvolution() {
@@ -236,12 +289,22 @@ function loadEvolution() {
     dialogContentRef.innerHTML = evolutionChain.map((evoIndex, i) => {
         let html = `<img class="evo-sprites" title="${stringToCapital(pokemonObject[evoIndex.id].name)}" src="${pokemonObject[evoIndex.id].sprite}">`;
         if (i < evolutionChain.length - 1) {
-            let nextLevel = evolutionChain[i + 1].minLevel;
+            let nextStep = evolutionChain[i + 1];
+            let conditionText = "";
+            if (nextStep.minLevel) {
+                conditionText = `Lvl ${nextStep.minLevel}`;
+            } else if (nextStep.item) {
+                conditionText = stringToCapital(nextStep.item.replace("-", " "));
+            } else if (nextStep.minHappiness) {
+                conditionText = `Happiness ${nextStep.minHappiness}`;
+            } else {
+                conditionText = "?";
+            }
             html += `
-                <div class="evo-arrow-container">
-                <p>${nextLevel ? 'Lvl ' + nextLevel : '?'}</p>
-                    <span>↓</span>
-                </div>`;
+                    <div class="evo-arrow-container">
+                    <p>${conditionText}</p>
+                        <span>↓</span>
+                    </div>`;
         }
 
         return html;
@@ -250,7 +313,7 @@ function loadEvolution() {
 }
 
 async function getDialogData() {
-    if (!pokemonObject[currentIndex].evoChain || !pokemonObject[currentIndex].flavorText) {
+    if (!pokemonObject[currentIndex].evoChain && !pokemonObject[currentIndex].flavorText) {
         let speciesAPI = await (await fetch(`https://pokeapi.co/api/v2/pokemon-species/${currentIndex}/`)).json();
         let evoAPI = await (await fetch(`${speciesAPI.evolution_chain.url}`)).json();
         pokemonObject[currentIndex].flavorText = getDescription(speciesAPI.flavor_text_entries);
@@ -267,9 +330,16 @@ async function getDialogData() {
 function getEvoArray(chain, pokemonIDs = []) {
     let currentStep = {
         id: cutOutIndex(chain.species.url),
-        minLevel: chain.evolution_details && chain.evolution_details.length > 0
+        minLevel: chain.evolution_details.length > 0
             ? chain.evolution_details[0].min_level
-            : null
+            : null,
+        item: (chain.evolution_details.length > 0 && chain.evolution_details[0].item != null)
+            ? chain.evolution_details[0].item.name
+            : null,
+        minHappiness: chain.evolution_details.length > 0
+            ? chain.evolution_details[0].min_happiness
+            : null,
+
     };
     pokemonIDs.push(currentStep);
     if (chain.evolves_to.length > 0) {
